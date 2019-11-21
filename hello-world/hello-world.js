b4de48a799679567ebb5e9af6bdcb02a66aa6d16
iqrao <muna1289@gmail.com>
2019-11-21 09:54:02 -0800

    Initialize

---
+++ hello-world/hello-world.js
@@ -0,0 +1,12 @@
+//Hello World JS Exercism.io 
+
+//declared function
+hello = function() {return 'Hello, World!';}
+//used export statement so that it is accessible to test
+export function hello(){};
+
+//https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export
+
+
+
+
